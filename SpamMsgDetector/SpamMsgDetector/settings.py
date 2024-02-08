6f654c248cba9982df9225b34ab431c9f959e034
sakil28 <sakil390.28@gmail.com>
2024-02-08 21:21:58 +0600

    FirstDev

--- SpamMsgDetector/SpamMsgDetector/settings.py
+++ SpamMsgDetector/SpamMsgDetector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x0o$sz#@j_@uy*3o&z@3r9#64d!+*ol%84(5n&x@fucj)m70g5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
